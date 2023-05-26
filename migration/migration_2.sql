9eab7f351d05eb40db658840e5ccf232f43067d6
Roberts Kalvitis <r.kalvitis@gmail.com>
2023-05-26 22:28:49 +0300

    minor change

--- migration/migration_2.sql
+++ migration/migration_2.sql
@@ -9,3 +9,5 @@ alter table Pienakumi_Lietotajam  drop constraint FK__Pienakumi__lieto__628FA481
 alter table Pienakumi_Lietotajam drop column lietotajs_ID
 --add new column and new constriant
 alter table Pienakumi_Lietotajam add Pasakuma_Rikotajs_ID int foreign key REFERENCES Pasakuma_Rikotajs(Pasakuma_Rikotajs_ID)
+--changed column data type
+ALTER TABLE Pasakums ALTER COLUMN atrasanas_vieta nvarchar(255);
